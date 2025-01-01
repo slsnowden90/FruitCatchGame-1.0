3416ffef675d678e29ef587b7cbe631cfde4d3e1
ssnowden <slsnowden90@gmail.com>
2025-01-01 01:04:14 -0500

    Added GUI
    GUI and Logo added

--- GameOver.gd
+++ GameOver.gd
@@ -4,7 +4,7 @@ var s = Global.final_score
 var counters = Global.fruit_counters
 
 func _ready() -> void:
-	$FinalScoreLabel.text = "Score: %d" % Global.final_score
+	$FinalScoreLabel.text = "Final Score: %d" % Global.final_score
 	$FruitStatsLabel.text = "Apples: %d\nRaspberries: %d" % [
 		Global.fruit_counters["apple"],
 		Global.fruit_counters["raspberry"]
@@ -16,3 +16,7 @@ func _on_quit_button_pressed():
 
 func _on_restart_button_pressed():
 	get_tree().change_scene_to_file("res://Main.tscn")
+
+
+func _on_main_menu_pressed():
+	get_tree().change_scene_to_file("res://MainMenu.tscn")
